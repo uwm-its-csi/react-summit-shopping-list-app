e93cb170c82e0dfc7edb79d217b4bb653e8cd61c
Robert Beerman <rbeerma@uw.edu>
2023-10-18 21:34:32 -0700

    Implement list picker

--- src/components/FormComponent.jsx
+++ src/components/FormComponent.jsx
@@ -1,8 +1,12 @@
-import { useContext, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { TextField, Button, IconButton } from '@mui/material';
+import Autocomplete, { createFilterOptions } from '@mui/material/Autocomplete';
 import AddIcon from '@mui/icons-material/Add';
 import RemoveIcon from '@mui/icons-material/Remove';
 import { AppContext } from '../root/AppContext';
+import { itemListUrl } from '../constants';
+
+const filter = createFilterOptions();
 
 export const FormComponent = () => {
     const {
@@ -12,10 +16,17 @@ export const FormComponent = () => {
 
     const [itemName, setItemName] = useState('');
     const [quantity, setQuantity] = useState(1);
-    
+    const [itemList, setItemList] = useState([]);
 
-    const handleChangeItem = (e) => {
-        setItemName(e.target.value);
+    const handleChangeItem = (e, newValue) => {
+        if (typeof newValue === 'string') {
+            setItemName(newValue);
+        } else if (newValue && newValue.inputValue) {
+            // Create a new value from the user input
+            setItemName(newValue.inputValue);
+        } else {
+            setItemName(newValue);
+        }
     }
 
     const handleChangeQuantity = (e) => {
@@ -47,15 +58,73 @@ export const FormComponent = () => {
             return prevQuantity - 1;
         });
     }
+   
+    useEffect(() => {
+        async function fetchItemList() {
+            const response = await fetch(itemListUrl);
+            const list = await response.json();
+            console.log(JSON.stringify(list));
+            setItemList(list);
+        }
+
+        fetchItemList();
+    }, []);
 
     return (
         <>
             <form>
-                <TextField id="item-name"
-                           label='Item Name'
-                           onChange={handleChangeItem}
-                           value={itemName} />
-                &nbsp;
+                <Autocomplete
+                    id="item-name"
+                    label='Item Name'
+                    onChange={(event, newValue) => {
+                        console.log(`newValue: ${newValue}`);
+                        if (typeof newValue === 'string') {
+                            setItemName(newValue);
+                        } else if (newValue && newValue.inputValue) {
+                            // Create a new value from the user input
+                            setItemName(newValue.inputValue);
+                        } else {
+                            setItemName(newValue);
+                        }
+                    }}
+                    value={itemName}
+                    filterOptions={(options, params) => {
+                        const filtered = filter(options, params);
+                        const { inputValue } = params;
+                        //Suggest the creation of a new value
+                        const isExisting = options.some((option) => inputValue === option.name);
+                        if (inputValue !== '' && !isExisting) {
+                            filtered.push({
+                                inputValue,
+                                name: `Add "${inputValue}"`,
+                            });
+                        }
+
+                        return filtered;
+                    }}
+                    selectOnFocus
+                    clearOnBlur
+                    handleHomeEndKeys
+                    options={itemList}
+                    getOptionLabel={(option) => {
+                        // Value selected with enter, right from the input
+                        if (typeof option === 'string') {
+                            return option;
+                        }
+                        // Add "xxx" option created dynamically
+                        if (option.inputValue) {
+                            return option.inputValue;
+                        }
+                        // Regular option
+                        return option.name;
+                    }}
+                    renderOption={(props, option) => <li {...props}>{option.name}</li>}
+                    sx={{ width: 300 }}
+                    freeSolo
+                    renderInput={(params) => (
+                        <TextField {...params} label="Pick an item" />
+                    )}
+                />
                 <IconButton id="increment"
                             onClick={increment}>
                     <AddIcon />
@@ -79,3 +148,48 @@ export const FormComponent = () => {
         </>
     );
 }
+
+/* const itemList = [
+    {
+        "name": "Milk"
+    },
+    {
+        "name": "Bread"
+    },
+    {
+        "name": "Dog Food"
+    },
+    {
+        "name": "Cat Food"
+    },
+    {
+        "name": "Coffee"
+    },
+    {
+        "name": "Tea"
+    },
+    {
+        "name": "Fish"
+    },
+    {
+        "name": "Broccoli"
+    },
+    {
+        "name": "Bananas"
+    },
+    {
+        "name": "Apples"
+    },
+    {
+        "name": "Crackers"
+    },
+    {
+        "name": "Cookies"
+    },
+    {
+        "name": "Eggs"
+    },
+    {
+        "name": "Cheese"
+    }
+]; */
--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,2 @@
 export const shoppingListUrl = 'https://rbeerma-shoppinglist.azurewebsites.net/api/shoppinglists';
+export const itemListUrl = 'https://rbeerma-shoppinglist.azurewebsites.net/api/itemlist';
